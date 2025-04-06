7adb6a3889e42a976d08ee8e7e0f6ad376ce58e3
meng <1792766874@qq.com>
2025-04-06 17:02:21 +0800

    feat: 添加索引节点相关函数

--- file_system/src/block_cache.rs
+++ file_system/src/block_cache.rs
@@ -58,7 +58,7 @@ impl BlockCache {
         }
     }
 
-    pub fn read<T, V>(self, offset: usize, f: impl FnOnce(&T) -> V) -> V {
+    pub fn read<T, V>(&self, offset: usize, f: impl FnOnce(&T) -> V) -> V {
         f(self.get_ref(offset))
     }
 
